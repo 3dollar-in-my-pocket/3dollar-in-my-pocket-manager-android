413bc5c18496fb48436803e9ec4045294c3559b6
yunji <zion830@naver.com>
2022-04-01 17:48:37 +0900

    feat - common component 추가

--- common/src/main/java/app/threedollars/common/BaseFragment.kt
+++ common/src/main/java/app/threedollars/common/BaseFragment.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
+import app.threedollars.common.ext.repeatOnCreated
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.logEvent
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -14,16 +16,34 @@ abstract class BaseFragment<VM : BaseViewModel> : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setScreenViewEvent()
         observeData()
     }
 
+    private fun setScreenViewEvent() {
+        FirebaseAnalytics.getInstance(requireContext()).logEvent(FirebaseAnalytics.Event.SCREEN_VIEW) {
+            param(FirebaseAnalytics.Param.SCREEN_NAME, this::class.java.simpleName)
+            param(FirebaseAnalytics.Param.SCREEN_CLASS, this::class.java.simpleName)
+        }
+    }
+
     private fun observeData() {
-        lifecycleScope.launchWhenCreated {
+        viewLifecycleOwner.repeatOnCreated {
             launch {
                 viewModel.toastMsg.collect {
                     Toast.makeText(requireContext(), it, Toast.LENGTH_SHORT).show()
                 }
             }
+            launch {
+                viewModel.isLoading.collect {
+                    initLoadingEvent(it)
+                }
+            }
         }
+    }
+
+    open fun initLoadingEvent(isLoading: Boolean) {
+        // progress 이벤트 처리
     }
 }
