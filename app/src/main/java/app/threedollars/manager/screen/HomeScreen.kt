9c8bf046f05b5e9211fbdde64758576bc3be7197
Mino <k3000js@naver.com>
2022-05-17 00:06:48 +0900

    지도 줌버튼 삭제

--- app/src/main/java/app/threedollars/manager/screen/HomeScreen.kt
+++ app/src/main/java/app/threedollars/manager/screen/HomeScreen.kt
@@ -24,6 +24,7 @@ fun HomeScreen() {
     val mapView = remember {
         MapView(context).apply {
             getMapAsync { naverMap ->
+                naverMap.uiSettings.isZoomControlEnabled = false
             }
         }
     }
