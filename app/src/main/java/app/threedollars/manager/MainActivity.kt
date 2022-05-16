8fbc00f7e05c6379e984a589e0231965a59d484b
Mino <k3000js@naver.com>
2022-05-16 23:56:12 +0900

    BottomNav 구현

--- app/src/main/java/app/threedollars/manager/MainActivity.kt
+++ app/src/main/java/app/threedollars/manager/MainActivity.kt
@@ -3,8 +3,14 @@ package app.threedollars.manager
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.material.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import app.threedollars.manager.screen.*
 import androidx.compose.ui.tooling.preview.Preview
 import app.threedollars.manager.sign.ui.LoginButtons
 import dagger.hilt.android.AndroidEntryPoint
@@ -15,15 +21,46 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            SocialLoginScreen()
+//            MainScreenView()
+//            SocialLoginScreen()
         }
     }
 }
 
 @Preview
 @Composable
+fun MainScreenView() {
+    val navController = rememberNavController()
+    Scaffold(
+        bottomBar = { BottomNavigation(navController = navController) }
+    ) {
+        NavigationGraph(navController = navController)
+    }
+}
+
+@Composable
 fun SocialLoginScreen() {
     Column {
         LoginButtons()
     }
 }
+
+@Composable
+fun NavigationGraph(navController: NavHostController) {
+    NavHost(navController, startDestination = BottomNavItem.Home.screenRoute) {
+        composable(BottomNavItem.Home.screenRoute) {
+            HomeScreen()
+        }
+        composable(BottomNavItem.Category.screenRoute) {
+            CategoryScreen()
+        }
+        composable(BottomNavItem.AddStore.screenRoute) {
+            AddStoreScreen()
+        }
+        composable(BottomNavItem.MyPage.screenRoute) {
+            MyPageScreen()
+        }
+    }
+}
+
+
---
+++ app/src/main/java/app/threedollars/manager/screen/HomeScreen.kt
@@ -0,0 +1,58 @@
+package app.threedollars.manager.screen
+
+import android.os.Bundle
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.naver.maps.map.MapView
+import kotlinx.coroutines.launch
+
+@Composable
+fun HomeScreen() {
+    val context = LocalContext.current
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val coroutineScope = rememberCoroutineScope()
+
+    // Lifecycle 이벤트를 수신하기 위해 AndroidView의 밖에서 먼저 선언합니다.
+    // recomposition시에도 유지되어야 하기 때문에 remember { } 로 기억합니다.
+    val mapView = remember {
+        MapView(context).apply {
+            getMapAsync { naverMap ->
+            }
+        }
+    }
+
+    // LifecycleEventObserver를 구현하고, 각 이벤트에 맞게 MapView의 Lifecycle 메소드를 호출합니다.
+    val lifecycleObserver = remember {
+        LifecycleEventObserver { source, event ->
+            // CoroutineScope 안에서 호출해야 정상적으로 동작합니다.
+            coroutineScope.launch {
+                when (event) {
+                    Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+                    Lifecycle.Event.ON_START -> mapView.onStart()
+                    Lifecycle.Event.ON_RESUME -> mapView.onResume()
+                    Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+                    Lifecycle.Event.ON_STOP -> mapView.onStop()
+                    Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+                }
+            }
+        }
+    }
+
+    // 뷰가 해제될 때 이벤트 리스너를 제거합니다.
+    DisposableEffect(true) {
+        lifecycleOwner.lifecycle.addObserver(lifecycleObserver)
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(lifecycleObserver)
+        }
+    }
+
+    // 생성된 MapView 객체를 AndroidView로 Wrapping 합니다.
+    AndroidView(factory = { mapView })
+}
