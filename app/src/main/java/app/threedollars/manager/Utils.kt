94d3df9cfa4acbf6dcf3728b35976b604014558d
Mino <k3000js@naver.com>
2022-05-23 18:10:11 +0900

    현재 주소 표시 추가

--- app/src/main/java/app/threedollars/manager/Utils.kt
+++ app/src/main/java/app/threedollars/manager/Utils.kt
@@ -6,6 +6,8 @@ import android.content.Context
 import android.content.ContextWrapper
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.LocationManager
 import android.net.Uri
 import android.provider.Settings
@@ -14,9 +16,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import app.threedollars.data.db.DataStoreManager
+import com.naver.maps.geometry.LatLng
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
+import java.util.*
 
 fun Activity.requestPermissionIfNeeds(permission: String = ACCESS_FINE_LOCATION) {
     when {
@@ -54,6 +58,31 @@ fun Context.getActivity(): AppCompatActivity? {
         currentContext = currentContext.baseContext
     }
     return null
+}
+
+fun getCurrentLocationName(location: LatLng?): String? {
+    val notFindMsg = MainApplication.context().getString(R.string.location_no_address)
+    if (location == null) {
+        return notFindMsg
+    }
+
+    val geoCoder = Geocoder(MainApplication.context(), Locale.KOREA)
+    return try {
+        val addresses: List<Address> =
+            geoCoder.getFromLocation(location.latitude, location.longitude, 1)
+        if (addresses.isEmpty()) {
+            notFindMsg
+        } else {
+            with(addresses[0]) {
+                (0..maxAddressLineIndex).map {
+                    getAddressLine(it)
+                }
+            }.joinToString().substringAfter(" ")
+        }
+    } catch (e: Exception) {
+        Log.e("getCurrentLocationName", e.message ?: "")
+        notFindMsg
+    }
 }
 
 fun isLocationAvailable(): Boolean =
--- app/src/main/java/app/threedollars/manager/screen/HomeScreen.kt
+++ app/src/main/java/app/threedollars/manager/screen/HomeScreen.kt
@@ -4,22 +4,34 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import app.threedollars.manager.*
+import app.threedollars.manager.R
 import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.CameraAnimation
@@ -80,9 +92,24 @@ fun HomeScreen() {
     // 생성된 MapView 객체를 AndroidView로 Wrapping 합니다.
     ConstraintLayout {
 
-        val imageButton = createRef()
+        val (imageButton, box) = createRefs()
 
         AndroidView(factory = { mapView!! })
+
+        AddressRoundTextView(
+            Modifier
+                .padding(start = 20.dp, end = 20.dp, top = 44.dp)
+                .height(54.dp)
+                .fillMaxWidth()
+                .constrainAs(box) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    top.linkTo(parent.top)
+                }
+                .clip(RoundedCornerShape(16.dp))
+                .background(colorResource(id = R.color.white)),
+        )
+
         CurrentLocationButton(Modifier.constrainAs(imageButton) {
             end.linkTo(parent.end)
             top.linkTo(parent.top)
@@ -105,6 +132,36 @@ fun CurrentLocationButton(modifier: Modifier) {
             painter = painterResource(id = R.drawable.ic_current_location),
             contentDescription = null,
             tint = Color.Unspecified
+        )
+    }
+}
+
+// TODO: 테스트 필요 버그확률 높음
+@Composable
+fun AddressRoundTextView(modifier: Modifier) {
+    Box(
+        modifier = modifier,
+        contentAlignment = Alignment.Center
+    ) {
+        val latLng = remember { mutableStateOf(currentPosition) }
+
+        val activity = LocalContext.current.getActivity()
+        val fusedLocationProviderClient = activity?.let {
+            LocationServices.getFusedLocationProviderClient(it)
+        }
+        if (isLocationAvailable() && isGpsAvailable()) {
+            val locationResult = fusedLocationProviderClient?.lastLocation
+            locationResult?.addOnSuccessListener {
+                if (it != null) {
+                    latLng.value = LatLng(it.latitude, it.longitude)
+                }
+            }
+        }
+        Text(
+            text = getCurrentLocationName(latLng.value)
+                ?: stringResource(id = R.string.location_no_address),
+            textAlign = TextAlign.Center,
+            fontSize = 16.sp
         )
     }
 }
