3562f6c3d8e8ae6b71a797316d2faf3019ff0478
Mino <k3000js@naver.com>
2022-05-23 15:34:21 +0900

    context 구현

--- app/src/main/java/app/threedollars/manager/MainApplication.kt
+++ app/src/main/java/app/threedollars/manager/MainApplication.kt
@@ -1,9 +1,22 @@
 package app.threedollars.manager
 
 import android.app.Application
+import android.content.Context
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        instance = this
+    }
 
+    companion object {
+        lateinit var instance: MainApplication
+            private set
+
+        fun context(): Context {
+            return instance.applicationContext
+        }
+    }
 }
