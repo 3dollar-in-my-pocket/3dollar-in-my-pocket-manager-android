595622e725120aa78cced732443c272acab0768e
Mino <k3000js@naver.com>
2022-06-07 17:51:10 +0900

    Home 화면 Bottom layout Ui 구현

--- app/src/main/java/app/threedollars/manager/screen/HomeScreen.kt
+++ app/src/main/java/app/threedollars/manager/screen/HomeScreen.kt
@@ -5,14 +5,9 @@ import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Icon
-import androidx.compose.material.IconButton
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -23,6 +18,7 @@ import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -40,7 +36,6 @@ import com.naver.maps.map.LocationTrackingMode
 import com.naver.maps.map.MapView
 import com.naver.maps.map.util.FusedLocationSource
 import kotlinx.coroutines.launch
-
 
 var currentPosition: LatLng? = null
 var mapView: MapView? = null
@@ -63,7 +58,6 @@ fun HomeScreen() {
             }
         }
     }
-
     // LifecycleEventObserver를 구현하고, 각 이벤트에 맞게 MapView의 Lifecycle 메소드를 호출합니다.
     val lifecycleObserver = remember {
         LifecycleEventObserver { source, event ->
@@ -91,30 +85,42 @@ fun HomeScreen() {
 
     // 생성된 MapView 객체를 AndroidView로 Wrapping 합니다.
     ConstraintLayout {
-
-        val (imageButton, box) = createRefs()
+        val (imageButton, box, bottomLayout) = createRefs()
 
         AndroidView(factory = { mapView!! })
 
         AddressRoundTextView(
             Modifier
-                .padding(start = 20.dp, end = 20.dp, top = 44.dp)
+                .padding(start = 20.dp, end = 20.dp)
                 .height(54.dp)
                 .fillMaxWidth()
                 .constrainAs(box) {
                     start.linkTo(parent.start)
                     end.linkTo(parent.end)
-                    top.linkTo(parent.top)
+                    top.linkTo(parent.top, margin = 44.dp)
                 }
                 .clip(RoundedCornerShape(16.dp))
-                .background(colorResource(id = R.color.white)),
+                .background(colorResource(id = R.color.white))
         )
 
         CurrentLocationButton(Modifier.constrainAs(imageButton) {
-            end.linkTo(parent.end)
-            top.linkTo(parent.top)
-            bottom.linkTo(parent.bottom)
+            end.linkTo(parent.end, margin = 16.dp)
+            bottom.linkTo(bottomLayout.top, margin = 16.dp)
         })
+
+        SalesLayout(
+            Modifier
+                .padding(bottom = 50.dp)
+                .fillMaxHeight(0.25f)
+                .fillMaxWidth()
+                .constrainAs(bottomLayout) {
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(parent.bottom)
+                }
+                .clip(RoundedCornerShape(16.dp))
+                .background(colorResource(id = R.color.white))
+        )
     }
 }
 
@@ -163,6 +169,59 @@ fun AddressRoundTextView(modifier: Modifier) {
             textAlign = TextAlign.Center,
             fontSize = 16.sp
         )
+    }
+}
+
+@Composable
+fun SalesLayout(modifier: Modifier) {
+    val sale = remember { mutableStateOf(false) }
+    Column(
+        modifier = modifier.background(
+            color = colorResource(id = if (sale.value) R.color.green500 else R.color.white)
+        )
+    ) {
+        Row(modifier = Modifier.padding(start = 26.dp, top = 24.dp)) {
+            Text(
+                text = if (sale.value) "오늘은" else "지금 계신 위치에서 영업을 시작할까요?",
+                fontWeight = FontWeight.Bold,
+                fontSize = 18.sp,
+                color = colorResource(id = if (sale.value) R.color.white else R.color.black)
+            )
+            if (sale.value) {
+                Text(
+                    modifier = Modifier
+                        .padding(start = 6.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(colorResource(id = R.color.green400)),
+                    text = "5시간 24분 23초",
+                    fontWeight = FontWeight.Bold,
+                    fontSize = 16.sp,
+                    color = colorResource(id = R.color.white)
+                )
+            }
+        }
+        Text(
+            modifier = Modifier.padding(start = 26.dp, top = 8.dp),
+            text = if (sale.value) "동안 영업중이시네요! 오늘도 대박나세요!" else "영업을 시작하면 위치가 손님들에게 공개됩니다.",
+            fontWeight = if (sale.value) FontWeight.Bold else FontWeight.Normal,
+            fontSize = if (sale.value) 18.sp else 14.sp,
+            color = colorResource(id = if (sale.value) R.color.white else R.color.black)
+        )
+        Button(
+            colors = ButtonDefaults.buttonColors(backgroundColor = colorResource(id = if (sale.value) R.color.white else R.color.green500)),
+            onClick = { sale.value = !sale.value },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 24.dp, vertical = 28.dp)
+                .clip(RoundedCornerShape(8.dp))
+        ) {
+            Text(
+                modifier = Modifier.padding(vertical = 8.dp),
+                text = if (sale.value) "셔터 내리기" else "오늘의 영업 시작하기",
+                fontSize = 16.sp,
+                color = colorResource(id = if (sale.value) R.color.green500 else R.color.white)
+            )
+        }
     }
 }
 
