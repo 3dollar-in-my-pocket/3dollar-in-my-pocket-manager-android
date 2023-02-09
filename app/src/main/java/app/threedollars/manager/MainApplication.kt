7515fb0a1abbbf484236b2c2c74f17271c66c0e9
Mino <k3000js@gmail.com>
2023-02-09 16:03:16 +0900

    [feat] kakaoSdk init

--- app/src/main/java/app/threedollars/manager/MainApplication.kt
+++ app/src/main/java/app/threedollars/manager/MainApplication.kt
@@ -1,9 +1,13 @@
 package app.threedollars.manager
 
 import android.app.Application
+import com.kakao.sdk.common.KakaoSdk
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MainApplication : Application() {
-
+    override fun onCreate() {
+        super.onCreate()
+        KakaoSdk.init(this, BuildConfig.KAKAO_KEY)
+    }
 }
