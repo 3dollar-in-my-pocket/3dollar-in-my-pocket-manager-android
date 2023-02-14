40b20b058811f5aa41392f00000bc13eb0222c63
Mino <k3000js@gmail.com>
2023-02-14 16:11:17 +0900

    [feat] splash screen Ui 구현

--- app/src/main/java/app/threedollars/manager/MainActivity.kt
+++ app/src/main/java/app/threedollars/manager/MainActivity.kt
@@ -6,8 +6,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -19,13 +17,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import app.threedollars.manager.sign.LoginNavItem
 import app.threedollars.manager.sign.ui.LoginScreen
 import app.threedollars.manager.sign.ui.SignScreen
+import app.threedollars.manager.sign.ui.SplashScreen
 import app.threedollars.manager.sign.viewmodel.LoginViewModel
 import com.kakao.sdk.auth.model.OAuthToken
 import com.kakao.sdk.common.model.ClientError
 import com.kakao.sdk.common.model.ClientErrorCause
 import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 
 @AndroidEntryPoint
@@ -105,8 +103,12 @@ fun LoginNavigationGraph(viewModel: LoginViewModel, loginKakao: () -> Unit) {
     val navController = rememberNavController()
 
     NavHost(
-        navController, startDestination = LoginNavItem.Login.screenRoute, modifier = Modifier.fillMaxSize()
+        navController, startDestination = LoginNavItem.Splash.screenRoute, modifier = Modifier.fillMaxSize()
     ) {
+        composable(LoginNavItem.Splash.screenRoute) {
+            SplashScreen(navController)
+        }
+
         composable(LoginNavItem.Login.screenRoute) {
             LoginScreen(navController, loginKakao, viewModel)
         }
--- app/src/main/java/app/threedollars/manager/sign/LoginNavItem.kt
+++ app/src/main/java/app/threedollars/manager/sign/LoginNavItem.kt
@@ -1,6 +1,7 @@
 package app.threedollars.manager.sign
 
 sealed class LoginNavItem(var screenRoute: String) {
+    object Splash : LoginNavItem("splash")
     object Login : LoginNavItem("login")
     object Sign : LoginNavItem("sign")
 }
