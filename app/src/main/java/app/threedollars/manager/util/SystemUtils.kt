ba6fc75ca8a5ed4ea5b9ba60e0764b1a9d7cac4c
Mino <k3000js@gmail.com>
2023-05-10 15:04:22 +0900

    [Feat] noRippleClickable 함수 추가

--- app/src/main/java/app/threedollars/manager/util/SystemUtils.kt
+++ app/src/main/java/app/threedollars/manager/util/SystemUtils.kt
@@ -1,9 +1,16 @@
 package app.threedollars.manager.util
 
+import android.app.Activity
 import android.content.Context
+import android.content.ContextWrapper
 import android.location.Address
 import android.location.Geocoder
 import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.composed
 import app.threedollars.manager.R
 import com.naver.maps.geometry.LatLng
 import java.util.*
@@ -28,5 +35,20 @@ fun Context.getCurrentLocationName(location: LatLng?): String {
     } catch (e: Exception) {
         Log.e("getCurrentLocationName", e.message ?: "")
         notFindMsg
+    }
+}
+fun Context.findActivity(): Activity {
+    var context = this
+    while (context is ContextWrapper) {
+        if (context is Activity) return context
+        context = context.baseContext
+    }
+    throw IllegalStateException("no activity")
+}
+
+inline fun Modifier.noRippleClickable(crossinline onClick: ()->Unit): Modifier = composed {
+    clickable(indication = null,
+        interactionSource = remember { MutableInteractionSource() }) {
+        onClick()
     }
 }
