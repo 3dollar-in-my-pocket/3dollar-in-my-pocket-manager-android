754a11747b7d04c627eb47d5c89601ea83c6bd6e
Mino <k3000js@gmail.com>
2023-06-08 14:59:37 +0900

    [Feat] 로그인 화면 디바이스 등록 api 추가

--- app/src/main/java/app/threedollars/manager/sign/viewmodel/LoginViewModel.kt
+++ app/src/main/java/app/threedollars/manager/sign/viewmodel/LoginViewModel.kt
@@ -6,16 +6,21 @@ import app.threedollars.common.EventFlow
 import app.threedollars.common.MutableEventFlow
 import app.threedollars.domain.usecase.AuthUseCase
 import app.threedollars.domain.usecase.BossAccountUseCase
+import app.threedollars.domain.usecase.BossDeviceUseCase
 import app.threedollars.manager.sign.LoginNavItem
+import com.google.firebase.messaging.FirebaseMessaging
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val authUseCase: AuthUseCase,
-    private val bossAccountUseCase: BossAccountUseCase
+    private val bossAccountUseCase: BossAccountUseCase,
+    private val bossDeviceUseCase: BossDeviceUseCase
 ) : BaseViewModel() {
 
     private val _loginNavItem = MutableEventFlow<LoginNavItem>()
@@ -41,7 +46,10 @@ class LoginViewModel @Inject constructor(
         viewModelScope.launch(exceptionHandler) {
             bossAccountUseCase.getBossAccount().collect {
                 if (it.code.toString() == "200") {
-                    _loginNavItem.emit(LoginNavItem.Home)
+                    val firebaseToken = async { FirebaseMessaging.getInstance().token.await() }
+                    bossDeviceUseCase.putBossDeviceToken("FCM", firebaseToken.await()).collect {
+                        _loginNavItem.emit(LoginNavItem.Home)
+                    }
                 } else if (it.code.toString() == "403") {
                     _loginNavItem.emit(LoginNavItem.Waiting)
                 } else if (it.code.toString() == "404") {
