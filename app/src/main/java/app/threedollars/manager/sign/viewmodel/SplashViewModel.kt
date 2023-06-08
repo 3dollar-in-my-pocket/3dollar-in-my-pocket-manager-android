8b129a6cbf1a0cbd50a1d3ef97642e126bd7f96f
Mino <k3000js@gmail.com>
2023-06-08 14:59:51 +0900

    [Feat] 스플래시 화면 디바이스 등록 api 추가

--- app/src/main/java/app/threedollars/manager/sign/viewmodel/SplashViewModel.kt
+++ app/src/main/java/app/threedollars/manager/sign/viewmodel/SplashViewModel.kt
@@ -6,19 +6,25 @@ import app.threedollars.common.EventFlow
 import app.threedollars.common.MutableEventFlow
 import app.threedollars.domain.usecase.AuthUseCase
 import app.threedollars.domain.usecase.BossAccountUseCase
+import app.threedollars.domain.usecase.BossDeviceUseCase
 import app.threedollars.manager.sign.LoginNavItem
+import com.google.firebase.messaging.FirebaseMessaging
 import com.kakao.sdk.auth.AuthApiClient
 import com.kakao.sdk.auth.TokenManager
 import com.kakao.sdk.user.UserApiClient
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.async
+import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.firstOrNull
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
     private val authUseCase: AuthUseCase,
-    private val bossAccountUseCase: BossAccountUseCase
+    private val bossAccountUseCase: BossAccountUseCase,
+    private val bossDeviceUseCase: BossDeviceUseCase
 ) : BaseViewModel() {
 
     private val _loginNavItem = MutableEventFlow<LoginNavItem>()
@@ -48,7 +54,10 @@ class SplashViewModel @Inject constructor(
         viewModelScope.launch(exceptionHandler) {
             bossAccountUseCase.getBossAccount().collect {
                 if (it.code.toString() == "200") {
-                    _loginNavItem.emit(LoginNavItem.Home)
+                    val firebaseToken = async { FirebaseMessaging.getInstance().token.await() }
+                    bossDeviceUseCase.putBossDeviceToken("FCM", firebaseToken.await()).collect {
+                        _loginNavItem.emit(LoginNavItem.Home)
+                    }
                 } else if (it.code.toString() == "401") {
                     autoLogin()
                 } else if (it.code.toString() == "403") {
