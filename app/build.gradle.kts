650a988075f512580d62ed8cbd83ff432607ffb4
Mino <k3000js@gmail.com>
2023-07-24 14:33:28 +0900

    [Release] v1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 23
         targetSdk = 33
         versionCode = 1
-        versionName = "0.0.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
