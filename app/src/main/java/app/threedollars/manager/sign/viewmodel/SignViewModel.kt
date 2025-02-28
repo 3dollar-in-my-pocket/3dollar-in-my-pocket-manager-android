031ccfd76802024c36e57c4ced077dca42f616c7
mino <k3000js@naver.com>
2025-02-28 16:48:58 +0900

    [Chore] - 불필요한 코드 삭제 및 수정

--- app/src/main/java/app/threedollars/manager/sign/viewmodel/SignViewModel.kt
+++ app/src/main/java/app/threedollars/manager/sign/viewmodel/SignViewModel.kt
@@ -9,9 +9,9 @@ import app.threedollars.domain.usecase.AuthUseCase
 import app.threedollars.domain.usecase.BossDeviceUseCase
 import app.threedollars.domain.usecase.ImageUploadUseCase
 import app.threedollars.domain.usecase.PlatformStoreCategoryUseCase
+import app.threedollars.manager.feature.storemanagement.model.StoreCategoriesVo
 import app.threedollars.manager.sign.LoginNavItem
 import app.threedollars.manager.util.dtoToVo
-import app.threedollars.manager.feature.storemanagement.model.StoreCategoriesVo
 import com.google.firebase.messaging.FirebaseMessaging
 import com.kakao.sdk.auth.TokenManager
 import dagger.hilt.android.lifecycle.HiltViewModel
